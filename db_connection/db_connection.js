5d2523946ca7e275615ceeaa4d4732a812650f02
Srikanth-gunja <srikanthgunja25@gmail.com>
2024-03-18 17:59:17 +0530

    db errror

--- db_connection/db_connection.js
+++ db_connection/db_connection.js
@@ -1,12 +1,15 @@
 const mongoose=require('mongoose');
 
+
 const DB_URL=process.env.DB_URL;
 const db_connect=async ()=>{
+	console.log(DB_URL)
 	try{
 		await mongoose.connect(DB_URL);
 		console.log("db connection established succesfully");
 	}
 	catch(err){
+		console.log("err:"+err);
 		console.log("db_not connected");
 	}
 }
